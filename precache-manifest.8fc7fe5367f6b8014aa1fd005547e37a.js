18e4fa77ca6f159018a76da6c0a7e76efe8031ec
Shanec92 <shanecox@MacBook-Air.local>
2020-09-24 07:08:01 -0400

    Updates

---
+++ precache-manifest.8fc7fe5367f6b8014aa1fd005547e37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3027b324d4841082ad28d98f7b49cf8b",
+    "url": "/Simple-Todo-App/index.html"
+  },
+  {
+    "revision": "8a439f8730e14c135aa1",
+    "url": "/Simple-Todo-App/static/css/main.d6a41ba8.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/Simple-Todo-App/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Todo-App/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a439f8730e14c135aa1",
+    "url": "/Simple-Todo-App/static/js/main.d8af6c2c.chunk.js"
+  },
+  {
+    "revision": "b7cbabd1aa620b4972d4",
+    "url": "/Simple-Todo-App/static/js/runtime-main.189dfa3e.js"
+  }
+]);
